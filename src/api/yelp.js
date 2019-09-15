194265dbf22e4edc7a744382b8fd9f2b1c439d45
VladYem <volodymyem@gmail.com>
2019-09-16 00:59:55 +0200

    Fixed search bug

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseUrl: 'https://api.yelp.com/v3/businesses',
+  baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
     Authorization:
       'Bearer jRoTkT_FjjUR9zezOLWCBZS2cI9m8_Qlj1ZSZ03Q_1GnE3P5FnzAgYNWHhfPUAAlrygwP-4OsDAxTMM-NDJWrsovP05TZ5WkPiMFU92atOWmjMvGcqTxkLualIN7XXYx'
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [restaurant, setResults] = useState([]);
+  const [restaurants, setResults] = useState([]);
 
-  const searchApi = async () => {
+  const searchApi = async searchTerm => {
+    console.log('hello there!');
     // see api yelp docs: https://www.yelp.com/developers/documentation/v3/business_search
     const response = await yelp.get('/search', {
       params: {
         limit: 50,
-        term,
+        term: searchTerm,
         location: 'krakow'
       }
     });
     setResults(response.data.businesses);
   };
 
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => searchApi()}
+        onTermSubmit={() => searchApi(term)}
       />
       <Text>Some Search will be here</Text>
-      <Text>We have found {restaurant.length} results</Text>
+      <Text>We have found {restaurants.length} results</Text>
     </View>
   );
 };
